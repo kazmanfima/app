60c6b35d4941f09591dcf73c5fa791c439bb44d7
kazmanfima <briantran@rocketmail.com>
2018-08-21 23:51:15 -0400

    button and bottom nav experimenting

--- app/src/main/java/com/example/brian/app/MainActivity.java
+++ app/src/main/java/com/example/brian/app/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.brian.app;
 
+import android.support.v4.app.Fragment;
+//import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.ActionBar;
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //int buttonDrawer = new int (R.layout.activity_main);
+
+    ActionBar navBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void searchActivity (View view){
+        Intent intent = new Intent (this, SearchDataActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity (intent);
+    }
+
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//        
+//    }
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/brian/app/SearchDataActivity.java
@@ -0,0 +1,13 @@
+package com.example.brian.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_data);
+    }
+}
